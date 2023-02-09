33b49f047b5dd443106a87c4771a164061c70abb
M.S.K Raghuteja <mskrt1388@gmail.com>
2023-02-09 01:37:52 +0000

    Update case_study_1.sql

--- case_study_1.sql
+++ case_study_1.sql
@@ -60,426 +60,220 @@ From Sales
 -- SOLUTIONS
 
 
-select * from dannys_diner.sales limit 5;
-select * from dannys_diner.menu limit 5;
-select * from dannys_diner.members limit 5;
+SELECT * FROM dannys_diner.members;
+SELECT * FROM dannys_diner.sales;
+SELECT * FROM dannys_diner.menu;
 
 
 --- 1. What is the total amount each customer spent at the restaurant?
 
----sales                      menu
+SELECT sales.customer_id, SUM(menu.price) FROM
+dannys_diner.sales AS sales
+LEFT JOIN
+dannys_diner.menu AS menu
+ON sales.product_id = menu.product_id
+GROUP BY sales.customer_id
 
----customer id
----product id          ----> product id
----                          price
----
+
+--- 2. How many days has each customer visited the restaurant?
+
+SELECT customer_id, COUNT(DISTINCT order_date) FROM
+dannys_diner.sales 
+GROUP BY customer_id;
 
 
-with customer_spendings as (
-select s.customer_id as cust_id ,s.product_id as prod_id, m.price as cost from 
-dannys_diner.sales as s inner join dannys_diner.menu as m
-ON
-s.product_id=m.product_id
+--- 3. What was the first item from the menu purchased by each customer?
+
+with customer_first_order AS (
+SELECT s.customer_id, s.order_date, 
+MIN(s.order_date) OVER (PARTITION BY S.customer_id) AS first_order_date,
+m.product_name 
+FROM dannys_diner.sales s
+INNER JOIN dannys_diner.menu m
+ON s.product_id = m.product_id 
 )
-select cust_id,sum(cost) as total_spendings from customer_spendings
-group by cust_id
-order by cust_id;
+SELECT  DISTINCT customer_id, product_name FROM customer_first_order
+WHERE order_date = first_order_date;
 
-
-SELECT sales.customer_id as cust_id,SUM(menu.price)
-from dannys_diner.sales
-INNER JOIN 
-dannys_diner.menu
-ON sales.product_id=menu.product_id
-group by cust_id;
-
-
-SELECT
-  sales.customer_id,
-  SUM(menu.price) AS total_sales
-FROM dannys_diner.sales
-INNER JOIN dannys_diner.menu
-ON sales.product_id=menu.product_id
-GROUP BY
-  sales.customer_id;
-  
-  
-  
---- How many days has each customer visited the restaurant?
-
----sales              
-
----customer id
----order_date
----product id          
-
-select customer_id,count( DISTINCT order_date) as number_of_days_visited from dannys_diner.sales
-group by customer_id;
-
---- What was the first item from the menu purchased by each customer?
-
----sales                      menu               members
-
----customer id                                    cust_id
----product id          ----> product id           joined_date  
----                          price
----order_date
-
-
-
----A   product_name
----B   product_name
-
-with cte_table as(
-select customer_id,product_name,
-RANK() over (partition by customer_id order by order_date) AS seq
-from dannys_diner.sales as s
-inner join
-dannys_diner.menu as m
-ON s.product_id=m.product_id
+-------------------------------------------- or ---------------------------------------
+with customer_first_order AS (
+SELECT s.customer_id, s.order_date, 
+RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS order_rank,
+m.product_name 
+FROM dannys_diner.sales s
+INNER JOIN dannys_diner.menu m
+ON s.product_id = m.product_id 
 )
-select DISTINCT customer_id,product_name from cte_table 
-where seq=1;
-
-# MAIN QEURY
-WITH ordered_sales AS (
-  SELECT
-    sales.customer_id,
-    -- does this look right?
-    RANK() OVER (
-      PARTITION BY sales.customer_id
-      ORDER BY sales.order_date
-    ) AS order_rank,
-    menu.product_name
-  FROM dannys_diner.sales
-  INNER JOIN dannys_diner.menu
-    ON sales.product_id = menu.product_id
-)
-SELECT DISTINCT
-  customer_id,
-  product_name
-FROM ordered_sales
--- what about this?
-WHERE order_rank = 1;
+SELECT  DISTINCT customer_id, product_name FROM customer_first_order WHERE order_rank = 1;
 
 
 
---- What is the most purchased item on the menu and how many times was it purchased by all customers?
-select m.product_name as prod_name,count(*) as freq
-from dannys_diner.sales as s
+--- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+
+select m.product_name, count(*) as freq_orders from dannys_diner.sales s
 INNER JOIN
-dannys_diner.menu as m
-ON s.product_id=m.product_id
-group by prod_name
-order by freq desc
-limit 1;
-
-# MAIN QEURY
-SELECT
-  menu.product_name,
-  COUNT(sales.*) AS total_purchases
-FROM dannys_diner.sales
-INNER JOIN dannys_diner.menu
-  ON
-sales.product_id=menu.product_id
-group by product_name
-ORDER BY total_purchases DESC
-LIMIT 1;
-
---- A product_name freq
---- B product_name freq
-
-
---- What is the most purchased item on the menu and how many times was it purchased by each customer?
-
-with freq_purchase as(
-select product_id,count(*) as purchase_freq
-from dannys_diner.sales
-group by product_id
-order by purchase_freq desc
-limit 1
-)
-select customer_id,product_id,count(*)
-from dannys_diner.sales 
-where product_id in (select product_id from freq_purchase)
-group by customer_id,product_id;
+dannys_diner.menu m
+ON s.product_id = m.product_id
+group by 1
+order by 2 desc limit 1
 
 
 
----Which i    tem was the most popular for each customer?
 
---- customer_id   product_name
---- cistomer_id   product_name
+--- 5. Which item was the most popular for each customer?
 
-# MAIN QEURY
-with cte_table as (
-select s.customer_id as cust,m.product_name as prod,count(*) as freq
----RANK() OVER (PARTITION BY cust order by freq) as _rank
-from 
-dannys_diner.sales as s
-INNER JOIN
-dannys_diner.menu as m
-ON s.product_id=m.product_id
-group by cust,prod
+with products_freq as (
+SELECT s.customer_id,m.product_name, count(*) as freq_orders
+FROM dannys_diner.sales as s
+INNER JOIN dannys_diner.menu as m
+ON s.product_id = m.product_id
+group by s.customer_id,m.product_name
 ),
-cte_table1 as( 
-select cust,prod,freq,
-RANK() OVER (PARTITION BY cust order by freq desc) as _rank
-from cte_table
+products_freq_rank as (
+SELECT customer_id,product_name,freq_orders,
+RANK() OVER (PARTITION BY customer_id ORDER BY freq_orders desc) as purchase_rank
+FROM products_freq 
 )
-select cust,prod,freq from cte_table1
-where _rank=1;
+SELECT customer_id,product_name,freq_orders FROM products_freq_rank where purchase_rank=1
 
-# MAIN QEURY
-with cte_table as (
-select s.customer_id as cust,m.product_name as prod,count(*) as freq,
-RANK() OVER (PARTITION BY s.customer_id order by count(*) desc) as _rank
-from 
-dannys_diner.sales as s
+-------------------------------or---------------------------------------------------------------
+
+with products_freq as (
+SELECT s.customer_id,m.product_name, count(*) as freq_orders,
+RANK() OVER (PARTITION BY customer_id ORDER BY count(*) desc) as purchase_rank
+FROM dannys_diner.sales as s
+INNER JOIN dannys_diner.menu as m
+ON s.product_id = m.product_id
+group by s.customer_id,m.product_name
+)
+SELECT customer_id,product_name,freq_orders FROM products_freq where purchase_rank=1
+
+
+--- 6. Which item was purchased first by the customer after they became a member?
+
+WITH customer_purchases as (
+SELECT s.customer_id,s.order_date,mem.join_date,m.product_name,
+RANK() OVER (PARTITION BY s.customer_id ORDER BY s.order_date) as purchase_rank
+FROM dannys_diner.sales s
 INNER JOIN
-dannys_diner.menu as m
-ON s.product_id=m.product_id
-group by cust,prod
-)
-select cust,prod,freq from cte_table
-where _rank=1;
-
-
-
-
---- most purchased product and how many times purchased by each customer 
-# PRACTICE
-with most_purchased_product as (
-select product_id,count(*) as freq
-from dannys_diner.sales
-group by product_id
-order by freq desc
-limit 1),
-
-# MAIN QEURY
-customerwise_purchases as(
-select customer_id,product_id,count(*) as prod_freq
-from dannys_diner.sales where product_id in (select product_id from most_purchased_product)
-group by 1,2)
-
-select customerwise_purchases.customer_id,menu.product_name,prod_freq
-from customerwise_purchases
-inner join
-dannys_diner.menu
-ON customerwise_purchases.product_id=menu.product_id
-
-
-
---- Which item was purchased first by the customer?
-
-# PRACTICE
-with ordered_table as (
-select customer_id,product_id,
-RANK() OVER (PARTITION BY customer_id order by order_date) as _rank
-from dannys_diner.sales
-)
-select DISTINCT customer_id,product_id from ordered_table
-where _rank=1;
-
-
-#MAIN QUERY
-with ordered_table as (
-select s.customer_id,m.product_name,
-RANK() OVER (PARTITION BY customer_id order by order_date) as _rank
-from dannys_diner.sales as s
-INNER JOIN 
-dannys_diner.menu as m
-ON s.product_id=m.product_id
-)
-select DISTINCT customer_id,product_name from ordered_table
-where _rank=1;
-
-
---- Which item was purchased first by the customer after they became a member?
-
-select * from dannys_diner.members;
-
-with cte_table as (
-select s.customer_id as cust_id,s.order_date  as dates,me.product_name as prod_name,
-RANK() OVER (PARTITION BY s.customer_id order by s.order_date) as _rank
-from dannys_diner.sales as s
-INNER JOIN 
-dannys_diner.members as m
-ON s.customer_id=m.customer_id
-INNER JOIN 
-dannys_diner.menu as me
-ON s.product_id=me.product_id
-WHERE s.order_date>=m.join_date
-)
-select cust_id,dates,prod_name from cte_table where _rank=1;
-
-
-select * from dannys_diner.members;
-
-
---- Which item was purchased just before the customer became a member?
-with cte_table as(
-select s.customer_id,s.order_date,me.product_name,
-RANK() over (partition by s.customer_id order by order_date desc) as _rank
-from dannys_diner.sales as s
+dannys_diner.menu m
+ON m.product_id = s.product_id
 INNER JOIN
-dannys_diner.members as m
-ON s.customer_id=m.customer_id
-INNER JOIN 
-dannys_diner.menu as me
-ON s.product_id=me.product_id
-WHERE s.order_date<m.join_date
+dannys_diner.members mem
+ON mem.customer_id = s.customer_id
+WHERE s.order_date>=mem.join_date
 )
-select customer_id,order_date,product_name
-from cte_table
-where
-_rank=1
-order by product_name;
+SELECT customer_id,order_date,product_name FROM customer_purchases
+where purchase_rank = 1;
 
 
+--- 7. Which item was purchased just before the customer became a member?
 
----What is the total items and amount spent for each member before they became a member?
-
-select s.customer_id as cust_id,count(DISTINCT s.product_id) as tot_items,SUM(m.price) as tot_price from
-dannys_diner.sales as s
+WITH customer_purchases as (
+SELECT s.customer_id,s.order_date,mem.join_date,m.product_name,
+RANK() OVER (PARTITION BY s.customer_id ORDER BY s.order_date desc) as purchase_rank
+FROM dannys_diner.sales s
 INNER JOIN
-dannys_diner.menu as m
-ON s.product_id=m.product_id
+dannys_diner.menu m
+ON m.product_id = s.product_id
 INNER JOIN
-dannys_diner.members as me
-ON s.customer_id=me.customer_id
-where s.order_date<me.join_date
-group by s.customer_id;
+dannys_diner.members mem
+ON mem.customer_id = s.customer_id
+WHERE s.order_date<mem.join_date
+)
+SELECT customer_id,order_date,product_name FROM customer_purchases
+where purchase_rank = 1;
 
 
 
+--- 8. What is the total items and amount spent for each member before they became a member?
 
----If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+SELECT s.customer_id, count(DISTINCT S.product_id) as total_items,
+sum(m.price) as amount_spent
+FROM dannys_diner.sales s
+INNER JOIN
+dannys_diner.menu m
+ON m.product_id = s.product_id
+INNER JOIN
+dannys_diner.members mem
+ON mem.customer_id = s.customer_id
+WHERE s.order_date<mem.join_date
+group by s.customer_id
+
+
+
+--- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 
 SELECT s.customer_id,
-SUM(CASE
-WHEN m.product_name='sushi'
-THEN (m.price)*20
-else
-(m.price)*10
-end)
-as tot_points
-FROM 
-dannys_diner.sales as s
-INNER JOIN
-dannys_diner.menu as m
-ON s.product_id=m.product_id
-group by 1;
-
-
-
---- In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
-
-select s.customer_id as cust_id,
 SUM(
-CASE WHEN s.order_date BETWEEN m.join_date and m.join_date+6 OR me.product_name='sushi'
-THEN
-20*me.price
-ELSE 
-10*me.price
-END
-)
-as tot_points
-from 
-dannys_diner.sales as s
-INNER JOIN
-dannys_diner.members as m
-ON s.customer_id=m.customer_id
-INNER JOIN
-dannys_diner.menu as me
-ON s.product_id=me.product_id
-WHERE s.order_date>='2021-01-01' and s.order_date<='2021-01-31'
-group by 1;
-
-
----Recreate the following table output using the available data:
-
-DROP TABLE if EXISTS SUMMARY;
-CREATE TEMP TABLE SUMMARY AS
-select s.customer_id as cust_id,s.order_date as dates,m.product_name as prod_name,
-m.price as prod_price,
-CASE WHEN me.customer_id is NOT NULL AND s.order_date>=me.join_date 
-THEN 'Y'
+CASE WHEN m.product_name='sushi' THEN
+2*10*m.price
 ELSE
-'N'
-END
-AS mem
-from 
-dannys_diner.sales AS s
+10*m.price 
+end) as points
+FROM dannys_diner.sales s
 INNER JOIN
-dannys_diner.menu AS m
-ON s.product_id=m.product_id
-LEFT JOIN
-dannys_diner.members as me
-ON s.customer_id=me.customer_id
-ORDER BY cust_id,dates,prod_name;
-
-select * from SUMMARY;
+dannys_diner.menu m
+ON m.product_id = s.product_id
+group by s.customer_id
 
 
+--- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
----Danny also requires further information about the ranking of customer products, but he purposely does not need the ranking for non-member purchases so he expects null ranking values for the records when customers are not yet part of the loyalty program.
+SELECT s.customer_id,
+SUM(
+CASE WHEN s.order_date BETWEEN (mem.join_date) AND (mem.join_date::date + 6) THEN
+2*10*m.price
+WHEN m.product_name = 'sushi' THEN
+2*10*m.price
+ELSE
+10*m.price
+end) as points
+FROM dannys_diner.sales s
+INNER JOIN
+dannys_diner.menu m
+ON m.product_id = s.product_id
+INNER JOIN
+dannys_diner.members mem
+ON mem.customer_id = s.customer_id
+where s.order_date>='2021-01-01'::Date and s.order_date<='2021-01-31'::Date 
+group by s.customer_id
 
-# PRACTICE
-select s.customer_id,s.order_date,
-m.product_name,m.price,
-CASE WHEN s.customer_id IS NOT NULL and s.order_date>=me.join_date
-THEN
+
+--- Recreating the table by joining all tables.
+
+DROP TABLE IF EXISTS SUMMARY; 
+CREATE TEMP TABLE SUMMARY AS (
+SELECT s.customer_id,s.order_date,m.product_name,m.price,
+CASE WHEN s.order_date < mem.join_date or mem.customer_id is NULL THEN
+'N'
+ELSE
 'Y'
-ELSE
-'N'
 END
-AS mem,
-CASE WHEN s.order_date<me.join_date or me.customer_id is NULL
-THEN NULL
-ELSE
-RANK() OVER (PARTITION BY s.customer_id order by s.order_date)
-END
-as ranking
-from 
-dannys_diner.sales AS s
+AS member
+FROM dannys_diner.sales s
 INNER JOIN
-dannys_diner.menu AS m
-ON s.product_id=m.product_id
+dannys_diner.menu m
+ON m.product_id = s.product_id
 LEFT JOIN
-dannys_diner.members as me
-ON s.customer_id=me.customer_id
-ORDER BY 1,2,3
+dannys_diner.members mem
+ON mem.customer_id = s.customer_id
+ORDER BY s.customer_id, s.order_date
+);
+
+SELECT * FROM SUMMARY;
 
 
+--- Danny also requires further information about the ranking of customer products, but he purposely does not need the ranking for non-member purchases so he expects null ranking values for the records when customers are not yet part of the loyalty program.
 
+SELECT *,
+CASE WHEN member='N' THEN
+null
+else
+RANK() OVER (PARTITION BY customer_id,member order by order_date) 
+end
+as Ranking
+FROM SUMMARY;
 
-# MAIN QUERY
-with cte_table as(
-select s.customer_id as cust_id,s.order_date as od,me.join_date as jd,
-m.product_name as prod_name,m.price as prod_price,
-CASE WHEN s.customer_id IS NOT NULL and s.order_date>=me.join_date
-THEN
-'Y'
-ELSE
-'N'
-END
-AS mem
-from 
-dannys_diner.sales AS s
-INNER JOIN
-dannys_diner.menu AS m
-ON s.product_id=m.product_id
-LEFT JOIN
-dannys_diner.members as me
-ON s.customer_id=me.customer_id
-)
-select cust_id,od,prod_name,prod_price,mem,
-CASE WHEN mem='N' OR od<jd 
-THEN NULL 
-ELSE
-RANK() OVER (PARTITION BY cust_id,mem order by od)
-END
-AS ranking
-from cte_table;
 
